4aaf313e38540f7c7863789f652e1e695a8b124d
Andrew <kolbe1129@gmail.com>
2021-10-07 16:17:48 -0500

    added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     # the name and email fields can't be a blank string and can't be too long
     validates :name, presence: true, length: { maximum: 60 }
-    validates :email, presence: true, length: { maximum: 255 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
