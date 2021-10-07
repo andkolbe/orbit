d0542f29778163acd47144ce84f71a2e091b61e7
Andrew <kolbe1129@gmail.com>
2021-10-07 14:51:57 -0500

    refactored url route paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'html_pages#home'
-  get 'html_pages/home'
-  get 'html_pages/help'
-  get 'html_pages/about'
-  get 'html_pages/contact'
+  get '/help', to: 'html_pages#help' # html_pages#help is the controller and the action. we are mapping the url /help to that controller and action
+  get '/about', to: 'html_pages#about'
+  get '/contact', to: 'html_pages#contact'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
