50f6c94338637e7a71c36cb8655bd530d6b57d40
Andrew <kolbe1129@gmail.com>
2021-10-08 13:57:56 -0500

    added a gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
+# in Rails, any methods defined in the helper file are available in any corresponding view
 module UsersHelper
+    def gravatar_for(user)
+        # gravatar urls are based on md5 hashes of the user's email. 
+        # In Ruby, the md5 algorithm is implemented using the hexdigest method, which is part of the digest library
+        # our helper code, returns an image tag for the gravatar with a gravatar css class and all text equal to the user's name
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase) # we want the argument to hexdigest to be all lowercase
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
 end
