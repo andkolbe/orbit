e47930551467a76cd187a0e2fa5d6f283f96d0d5
Andrew <kolbe1129@gmail.com>
2021-10-08 13:28:19 -0500

    added show action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show # GET requests for one user are handled by the show action
+      # create an instance variable that can be used as @user in the users/show view
+      # the controller reaches out to the model to find the user by the id in the database and then pass that to the view
+      @user = User.find(params[:id])
+  end
 end
