ccce48626fd46c5151178be8ff7cb820e15db534
Andrew <kolbe1129@gmail.com>
2021-10-08 14:16:52 -0500

    started invalid signups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,17 @@ class UsersController < ApplicationController
   end
 
   def show # GET requests for one user are handled by the show action
-      # create an instance variable that can be used as @user in the users/show view
-      # the controller reaches out to the model to find the user by the id in the database and then pass that to the view
-      @user = User.find(params[:id])
+    # create an instance variable that can be used as @user in the users/show view
+    # the controller reaches out to the model to find the user by the id in the database and then pass that to the view
+    @user = User.find(params[:id])
   end
 
-  
+  def create
+    @user = User.new[params[:user]]
+    if @user.save
+      # Handle a successful save
+    else 
+      render 'new'
+    end
+  end
 end
