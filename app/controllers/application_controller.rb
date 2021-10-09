dddeeb16f6808e20a806c09f29489fe251aaffca
Andrew <kolbe1129@gmail.com>
2021-10-09 09:40:16 -0500

    added the login user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "Hello World"
-    end
+    include SessionsHelper # makes the sessions helper available for all of the controllers
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+    def log_in(user)
+        # creates an encrypted session cookie off of the id of the user
+        session[:user_id] = user.id
+    end
 end
