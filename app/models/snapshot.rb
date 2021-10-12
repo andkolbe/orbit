493310319389e559b179ddcb775a947f90a20c22
Andrew <kolbe1129@gmail.com>
2021-10-12 10:57:26 -0500

    added ability to resize images

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -6,4 +6,8 @@ class Snapshot < ApplicationRecord
   validates :content, presence: true, length: { maximum: 250 }
   validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "Please upload a  valid file type (jpeg, gif, png)."},
                                   size: { less_than: 5.megabytes, message: "Your image exceeds 5MB." }
+  
+  def display_image
+    image.variant(resize_to_limit: [500, 500])
+  end
 end
